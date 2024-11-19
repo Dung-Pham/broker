0bc04d9f81b1f2cba41f74f1e659985fa79a9762
Dung-Pham <phamdunglili04012003@li04012003gmail.12003gmailcom>
2024-11-19 08:56:17 +0700

    add broker

---
+++ client-broker2.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as mqtt
+
+BROKER_ADDRESS = "192.168.1.52"
+PORT = 1883
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    publish_message("dev002/data/air", "123")
+    publish_message("dev002/data/light", "456")
+    publish_message("dev002/data/rain", "789")
+
+def on_message(client, userdata, message):
+    print(f"Message received on {message.topic}: {message.payload.decode()}")
+
+def publish_message(topic, message):
+    client.publish(topic, message)
+    print(f"Message published to {topic}: {message}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(BROKER_ADDRESS, PORT)
+client.subscribe("dev002/led")  
+client.subscribe("dev002/toggle") 
+print("hihi")
+client.loop_forever()
